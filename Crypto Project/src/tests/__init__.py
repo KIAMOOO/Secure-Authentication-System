1468f69176e0ea027e39a7d0b840442dcd6d16fe
ADIL <138507177+KIAMOOO@users.noreply.github.com>
2025-12-03 13:14:02 +0500

    Delete Crypto Project directory

--- Crypto Project/src/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Unit tests for the Secure Authentication System.
-"""
-
-# Test files will be added here
-# Example: test_auth_system.py, test_encryption.py, etc.
-
--- Crypto Project/src/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Utility functions for the Secure Authentication System.
-"""
-
-from .random_generator import generate_secure_random
-from .key_exchange import get_public_key_pem
-
-__all__ = ['generate_secure_random', 'get_public_key_pem']
-
